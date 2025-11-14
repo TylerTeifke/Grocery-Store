2fa4f0a74021529919c8f7145fbe1e0ccfb15f4c
TylerTeifke <tylerteifke@gmail.com>
2025-11-14 11:13:31 -0800

    Finished the list sub menu.

--- list_entries_menu.py
+++ list_entries_menu.py
@@ -14,6 +14,63 @@ def list_employees(cursor):
     for result in results:
         print('Name:', result[0], result[1] + ',', 'Position:', result[2] + ',', 'Salary: $' + str(result[3]))
 
+#Will print out the names of every customer
+def list_customers(cursor):
+    #The SQL statement that will get the relevant information about each customer
+    query = """SELECT first_name, last_name
+                FROM Customers;"""
+    
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    #Will print out all of the results from the SQL query above
+    for result in results:
+        print('Name:', result[0], result[1])
+
+#Will print out the names of each customer, as well as what they bought
+def list_purchases(cursor):
+    #The SQL statement that will get the relevant information about each customer and product
+    query = """SELECT customers.first_name, customers.last_name, product_details.name
+               FROM Purchases
+	                JOIN Customers ON Purchases.customer_id = Customers.id
+	                JOIN Products ON Purchases.product_id = Products.id
+	                JOIN Product_details ON Products.details_id = Product_details.id;"""
+    
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    #Will print out all of the results from the SQL query above
+    for result in results:
+        print('Name:', result[0], result[1] + ',', 'Purchase:', result[2])
+
+#Will print out the products that can be sold at the store
+def list_products(cursor):
+    #The SQL statement that will get the relevant information about each product
+    query = """SELECT name, type, price
+               FROM Product_Details
+	            JOIN Product_Types ON Product_Details.type_ID = Product_Types.id;"""
+    
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    #Will print out all of the results from the SQL query above
+    for result in results:
+        print('Name:', result[0] + ',', 'Type:', result[1] + ',', 'price: $' + str(result[2]))
+
+#Will print out the products that are actually available, as well as their expiration date
+def list_available_products(cursor):
+    #The SQL statement that will get the relevant information about each product
+    query = """SELECT name, expiration_date
+               FROM Products
+	            JOIN Product_Details ON Products.details_ID = Product_Details.ID;"""
+    
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    #Will print out all of the results from the SQL query above
+    for result in results:
+        print('Name:', result[0] + ',', 'Expiration Date:', result[1])
+
 def main(cursor):
     while True:
         print('Welcome to the list menu. Here you can see lists of the entries in each table of the database')
@@ -30,5 +87,13 @@ def main(cursor):
             print('')
         elif int(code) == 1:
             list_employees(cursor)
+        elif int(code) == 2:
+            list_customers(cursor)
+        elif int(code) == 3:
+            list_purchases(cursor)
+        elif int(code) == 4:
+            list_products(cursor)
+        elif int(code) == 5:
+            list_available_products(cursor)
         else:
             break
