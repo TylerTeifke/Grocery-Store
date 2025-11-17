2e62942116e2ee564e4ab995063024cd9dd122c6
TylerTeifke <tylerteifke@gmail.com>
2025-11-17 11:34:33 -0800

    Finished up the delete menu.

--- delete_entry_menu.py
+++ delete_entry_menu.py
@@ -24,23 +24,31 @@ def validate_id(id, table, cursor):
 
 #Will check to see if the entry ID is a foreign key in any other table
 def check_relations(id, current_table, foreign_table, cursor):
+    #Will be used to concatenate the name of the current table in the query
+    concat_table = ""
+
+    if current_table == 'Product_details':
+        concat_table = current_table[8:]
+    else:
+        concat_table = current_table[:len(current_table) - 1]
+
     #This query will look to see if there is any entity in a foreign table that is connected to the entity
     #in the current table
-    query = "SELECT * FROM " + current_table + " JOIN " + foreign_table + " ON " + current_table + ".id = " + foreign_table + "." + current_table[:len(current_table) - 1] + "_ID WHERE " + current_table[:len(current_table) - 1] + "_ID = ?;"
+    query = "SELECT * FROM " + current_table + " JOIN " + foreign_table + " ON " + current_table + ".id = " + foreign_table + "." + concat_table + "_ID WHERE " + concat_table + "_ID = ?;"
     value = [id]
 
     cursor.execute(query, value)
     results = cursor.fetchall()
 
     if len(results) > 0:
-        print('Unable to delete that entry as it is connected to another entry.')
+        print('Unable to delete that entry as it is connected to another entry in the ' + foreign_table + ' table.')
         print('Please delete the other entry and try again.')
         print('')
         return False
     
     return True
 
-#Will handle deleting an entry into any table except the purchases table
+#Will handle deleting an entry from the employee table
 def delete_employee(connection, cursor):
     can_continue = False
 
@@ -54,6 +62,83 @@ def delete_employee(connection, cursor):
 
     connection.commit()
 
+#Will handle deleting an entry from the customer table
+def delete_customer(connection, cursor):
+    can_continue = False
+
+    while not can_continue:
+        id = input('Enter Customer ID: ')
+        can_continue = validate_id(id, 'Customers', cursor) and check_relations(id, 'Customers', 'Purchases', cursor)
+
+    query = 'DELETE FROM Customers WHERE ID = ?'
+    value = [id]
+    cursor.execute(query, value)
+
+    connection.commit()
+
+#Will check to see if the customer and product ID's entered for deleting
+#a purchase are valid and are in the purchases table
+def validate_purchase(customer_id, product_id, cursor):
+    if customer_id == '' or not customer_id.isdecimal() or product_id == '' or not product_id.isdecimal():
+        print("One of the ID's is invalid. Try again")
+        print("")
+        return False
+    
+    query = "SELECT * FROM Purchases WHERE customer_ID = ? AND product_ID = ?"
+    values = (customer_id, product_id)
+    cursor.execute(query, values)
+    results = cursor.fetchall()
+
+    if len(results) < 1:
+        print("There are no purchases involving that customer and product. Try again.")
+        print("")
+        return False
+    
+    return True
+
+#Will handle deleting an entry from the purchase table
+def delete_purchase(connection, cursor):
+    can_continue = False
+
+    while not can_continue:
+        customer_id = input('Enter Customer ID: ')
+        product_id = input('Enter Product ID: ')
+        can_continue = validate_purchase(customer_id, product_id, cursor)
+
+    query = 'DELETE FROM Purchases WHERE customer_ID = ? AND product_ID = ?'
+    value = (customer_id, product_id)
+    cursor.execute(query, value)
+
+    connection.commit()
+
+#Will handle deleting an entry from the product_details table
+def delete_product(connection, cursor):
+    can_continue = False
+
+    while not can_continue:
+        id = input('Enter Product ID: ')
+        can_continue = validate_id(id, 'Product_details', cursor) and check_relations(id, 'Product_details', 'Products', cursor)
+
+    query = 'DELETE FROM Product_details WHERE ID = ?'
+    value = [id]
+    cursor.execute(query, value)
+
+    connection.commit()
+
+#Will handle deleting an entry from the products table
+def delete_from_inventory(connection, cursor):
+    can_continue = False
+
+    while not can_continue:
+        id = input('Enter Inventory ID: ')
+        can_continue = validate_id(id, 'Products', cursor) and check_relations(id, 'Products', 'Purchases', cursor)
+
+    query = 'DELETE FROM Products WHERE ID = ?'
+    value = [id]
+    cursor.execute(query, value)
+
+    connection.commit()
+
 def main(connection, cursor):
     while True:
 
@@ -62,7 +147,7 @@ def main(connection, cursor):
         print('Type [2] to delete a customer')
         print('Type [3] to delete a purchase')
         print('Type [4] to delete a product')
-        print('Type [5] to delete a copy of a product in the inventory')
+        print('Type [5] to delete a copy of a product from the inventory')
         print('Type [6] to quit')
         code = input('--> ')
         
@@ -71,5 +156,13 @@ def main(connection, cursor):
             print('')
         elif int(code) == 1:
             delete_employee(connection, cursor)
+        elif int(code) == 2:
+            delete_customer(connection, cursor)
+        elif int(code) == 3:
+            delete_purchase(connection, cursor)
+        elif int(code) == 4:
+            delete_product(connection, cursor)
+        elif int(code) == 5:
+            delete_from_inventory(connection, cursor)
         else:
             break
